686aa85d7b355d6a848965d47d6e637a7d769750
chenmingzhang <cmzhang1318@hotmail.com>
2019-01-28 15:59:32 +1000

    mp7 works

--- mp7.py
+++ mp7.py
@@ -1,3 +1,6 @@
+
+import matplotlib as mpl
+
 model_name=modelname
 mp_namea = model_name + 'a_mp'
 mp_nameb = model_name + 'b_mp'
@@ -21,8 +24,10 @@ pcoord = np.array([[0.000, 0.125, 0.500],
 #plocs = [nodew for i in range(pcoord.shape[0])]
 
 #plocs=np.ones(16)+10   np array is not working
-nodew=10
-plocs=[10]*16   # list is fine
+nodew=50
+plocs=[50]*16   # list is fine
+nodew=1000
+plocs=[1000]*16   # list is fine
 # create particle data
 pa = flopy.modpath.ParticleData(plocs, structured=False,
                                 localx=pcoord[:, 0],
@@ -70,3 +75,41 @@ mp.write_input()
 
 # run modpath
 mp.run_model()
+
+model_ws='.'
+fpth = os.path.join(model_ws, mp_namea + '.mppth')
+p = flopy.utils.PathlineFile(fpth)
+p0 = p.get_alldata()
+
+
+
+fpth = os.path.join(model_ws, mp_namea + '.timeseries')
+ts = flopy.utils.TimeseriesFile(fpth)
+ts0 = ts.get_alldata()
+
+# this two lines could never be shown properly. was it because this is an output for mf6?
+#fname = os.path.join(model_ws, model_name + '.disv.grb')
+#grd = flopy.utils.MfGrdFile(fname, verbose=False)
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1, aspect='equal')
+# everything associated with sr can not work at the moment
+modelmap = flopy.plot.ModelMap(model=ms,rotation=0)  #suggested by flopy3_MapExample.ipynb
+linecollection = modelmap.plot_grid()  # this line is the plotting line
+quadmesh = modelmap.plot_ibound()
+#ax.set_xlim(0, Lx)
+#ax.set_ylim(0, Ly)
+# mm is very important to trace all the particles
+mm = flopy.plot.ModelMap(sr=dis.sr, ax=ax)
+cmap = mpl.colors.ListedColormap(['r','g',])
+#v = mm.plot_cvfd(verts, iverts, edgecolor='gray', a=ibd, cmap=cmap)
+mm.plot_pathline(p0, layer='all', color='blue', lw=0.75)
+fig.show()
+#colors = ['green', 'orange', 'red']
+#for k in range(nlay):
+#        mm.plot_timeseries(ts0, layer=k, marker='o', lw=0, color=colors[k]);
+#fig.show()
+
+
+
+
